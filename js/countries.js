a06c2ff36ee12326e9fb323a9b550e1f198301a6
mehedi hasan <mehedicsebu@gmail.com>
2022-02-25 12:34:53 +0600

    country added

--- js/countries.js
+++ js/countries.js
@@ -14,13 +14,29 @@ const showCountries = (countries) => {
     countries.forEach(country => {
         const div = document.createElement('div');
         div.classList.add('country');
-        const h2 = document.createElement('h2');
-        const p = document.createElement('p');
-        h2.innerText = `Official Name: ${country.name.official}`;
-        p.innerText = `capital: ${country.capital}`;
-        div.appendChild(h2);
-        div.appendChild(p);
+        div.innerHTML = `
+        <h2>${country.name.common}</h2>
+        <p>${country.capital}</p>
+        <button onclick="showDetails('${country.name.common}')">Show Details</button>
+        `;
         countryDiv.appendChild(div);
     });
+}
+
+const showDetails = (name) => {
+    const url = `https://restcountries.com/v2/name/${name}`
+    //console.log(url);
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayCountry(data[0]))
+}
+const displayCountry = country => {
+    console.log(country);
+    const detailsDiv = document.getElementById('country-detais');
+    detailsDiv.innerHTML =
+        `<h2>Country: ${country.name}</h2>
+        <p> Area: ${country.area}</p>
+        <img width="200px" src="${country.flag}">
+   `;
 }
 
